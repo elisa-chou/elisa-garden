34711216aa913e5b5109e2e583afa8b897086657
Elisa <125857551+elisa-chou@users.noreply.github.com>
2026-08-27 14:11:54 +0800

    Quartz sync: Aug 27, 2026, 2:11 PM

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,8 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import { componentRegistry } from "./quartz/components/registry"
+import Stage from "./quartz/components/Stage"
+
+componentRegistry.register("Stage", Stage, "builtin")
 
 const config = await loadQuartzConfig()
 export default config
